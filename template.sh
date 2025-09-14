3bd1b553e4e62cdef1dfcefa99cd8b3f884ea65e
Krishna Gupta <krishna.gupta2023a@vitstudent.ac.in>
2025-09-15 01:25:10 +0530

    Folder structure

---
+++ template.sh
@@ -0,0 +1,15 @@
+#create directories
+mkdir -p src
+mkdir -p resources
+
+#creating files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resources/trials.ipynb 
+touch requirements.txt
+
+echo "Directories for folders and files are created successfully"
